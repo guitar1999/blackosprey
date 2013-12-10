9aa4498ae31657587773bad10f80d20d268d363c
Jesse Bishop <guitar1999@aol.com>
2013-12-10 06:55:27 -0500

    finding other sites

---
+++ stream_gauge/get_measurement_sites.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+import os, psycopg2, sys
+from datetime import datetime
+
+outdir = sys.argv[1]
+if outdir[-1] != '/':
+    outdir = outdir + '/'
+
+# Get db connection
+db = psycopg2.connect(host='localhost', database='blackosprey',user='jessebishop')
+cursor = db.cursor()
+
+# Get a list of huc ids that have avaricosa and gauge data
+sql = "SELECT distinct huc_8_num FROM nhd_hu8_watersheds n, usgs_streamgauges u WHERE contains_avaricosa = 'Y';"
+cursor.execute(sql)
+hucs = [this[0] for this in cursor.fetchall()]
+
+for huc in hucs:
+    surl = """http://waterservices.usgs.gov/nwis/site/?format=rdb&huc=%s""" % (huc)
+    os.system("""wget %s -O %ssites_in_huc_%s.csv""" % (durl.replace('&','\&'), outdir, huc))
+    
+cursor.close()
+db.close()

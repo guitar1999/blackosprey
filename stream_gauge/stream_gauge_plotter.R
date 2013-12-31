6d1024d7cb7d3044ac1c1dee2ffd631eae55a987
Jesse Bishop <guitar1999@aol.com>
2013-12-30 23:55:19 -0500

    new plotter

--- stream_gauge/stream_gauge_plotter.R
+++ stream_gauge/stream_gauge_plotter.R
@@ -10,5 +10,12 @@ query <- "SELECT avg(discharge_mean::numeric) AS avg, min(datetime) AS mdate, da
 
 df <- dbGetQuery(con, query)
 
-plot(df$mdate, df$avg, type='l')
+setwd('/Volumes/BlackOsprey/MapBook/StreamGauge/')
+fname <- 'huc_01080204_site_01173500_manual.pdf'
 
+pdf(file=fname, 10, 7.5)
+plot(df$mdate, df$avg, type='l', col='black', main='Watershed: Chicopee (1080204)\nSite: WARE RIVER AT GIBBS CROSSING, MA (01173500)\n\nMean Montly Stream Flow Measurement (USGS)', xlab='Measurement Date', ylab='Mean Discharge (cfs)', cex.main=0.90, cex.lab=0.80, cex.axis=0.80, cex=0.80)
+lm <- lm(df$avg ~ df$mdate)
+abline(lm, col='deepskyblue')
+legend("topright", paste("Trend",round(lm$coefficients[2] * 10, 3), "(cfs/decade)", sep=" "), lty=1, col="deepskyblue",bty="n", cex=0.80)
+dev.off()

cfdcccda0fc7c347259bb7fadb98a3a264f17a46
Jesse Bishop <guitar1999@aol.com>
2014-01-24 17:36:16 -0500

    mods

--- stream_gauge/stream_gauge_plotter.R
+++ stream_gauge/stream_gauge_plotter.R
@@ -1,5 +1,9 @@
-site <- '01173500'
-huc <- '01080204'
+#site <- '01173500'
+#huc <- '01080204'
+#site <- '01073260'
+site <- '01073600'
+huc <- '01060003'
+
 
 library(RPostgreSQL)
 
@@ -7,6 +11,8 @@ library(RPostgreSQL)
 con <- dbConnect(drv="PostgreSQL", host="127.0.0.1", user="jessebishop", dbname="blackosprey")
 
 query <- "SELECT avg(discharge_mean::numeric) AS avg, min(datetime) AS mdate, date_part('year', datetime) AS year, date_part('month', datetime) AS month FROM usgs_stream_gauge_daily where huc_8_num = 1080204 and site_no = 01173500 GROUP BY date_part('year', datetime), date_part('month', datetime) order by date_part('year', datetime), date_part('month', datetime); "
+
+query <- paste("SELECT min(discharge_mean::numeric) AS min, avg(discharge_mean::numeric) AS avg, max(discharge_mean::numeric) AS max, min(datetime) AS mdate, year, season FROM usgs_stream_gauge_seasons  WHERE huc_8_num = ", huc, " AND site_no = '", site, "' GROUP BY year, season ORDER BY year, CASE WHEN season = 'winter' THEN 1 WHEN season = 'spring' THEN 2 WHEN season = 'summer' THEN 3 ELSE 4 END;", sep='')
 
 df <- dbGetQuery(con, query)
 

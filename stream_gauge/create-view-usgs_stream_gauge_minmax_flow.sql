546e4e4bddee40b538e63651dbac60a4887142c6
Jesse Bishop <guitar1999@aol.com>
2014-01-26 10:03:32 -0500

    new view

---
+++ stream_gauge/create-view-usgs_stream_gauge_minmax_flow.sql
@@ -0,0 +1,13 @@
+BEGIN;
+CREATE VIEW usgs_stream_gauge_minmax_flow AS (
+    SELECT  
+        *,
+        CASE
+            WHEN date_part('month', datetime) IN (3,4,5) THEN
+                'max'
+            WHEN date_part('month', datetime) IN (8,9) THEN
+                'min'
+        END AS flow
+    FROM usgs_stream_gauge_daily
+);
+COMMIT;

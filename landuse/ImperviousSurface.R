ffe52587e70cc2117ad80e1511a9d4716d26583f
Tina Cormier <tcormier@whrc.org>
2014-03-02 21:37:19 -0500

    Added comments/documentation to climate and landuse scripts.

---
+++ landuse/ImperviousSurface.R
@@ -0,0 +1,46 @@
+# Purpose: This script contains functions that generate plots of % impervious surface from the NLCD 
+#          impervious surface layers.  
+# Author: Tina Cormier
+# Date: March 2, 2014
+# Notes: See comments for notes on needed changes.  
+# Status: In progress
+#
+#################################################################
+
+#This top stuff can go into a wrapper when finished testing. Need to add in DB stuff too.
+library(raster)
+library(maptools)
+library(rasterVis)
+
+#path/name of impervious surface layer
+imp <- "/Users/tcormier/Google\ Drive/wicklow/brook_floater/testing/NLCD2006_impervious_5-4-11/nlcd2006_impervious_5-4-11.img"
+
+#path/name of polygon file of the boundaries within which to summarize impervious surfaces.
+polyfile <- "/Users/tcormier/Google\ Drive/wicklow/brook_floater/testing/nhd_hu8_watersheds_clipped_avY_albers.shp"
+
+#read in imp.ras and poly *NOTE, before doing this analysis for real, clip to region (will be faster) once
+#hard drives are back online.
+imp.ras <- raster(imp)
+poly <- readShapePoly(polyfile)
+reg.crop <- crop(imp.ras, poly)
+
+for (i in c(1:length(poly))){
+  hucSum <- summImp(imp.ras, poly[i,])
+  
+    
+}# end poly loop
+
+#for testing
+poly <- poly[i]
+imp.ras <- reg.crop
+
+#Function to summarize impervious surfaces into a plot
+summImp <- function(imp.ras, poly) {
+  imp.crop <- crop(imp.ras, poly)
+  imp.mask <- raster::mask(imp.crop, poly)
+  
+  #plot - need to do some data exploration before I can decide
+  #how the plot should look.
+  summary(imp.mask)
+  
+} #end summImp function

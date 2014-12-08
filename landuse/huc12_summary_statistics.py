bb2d892580b2c1bd88d4bd6367dc2ef843321cb0
Jesse Bishop <guitar1999@aol.com>
2014-12-07 20:36:35 -0500

    takes a list or a huc

--- landuse/huc12_summary_statistics.py
+++ landuse/huc12_summary_statistics.py
@@ -39,6 +39,19 @@ def rasterize_huc(huc, edir, coordlist):
 
 #def summarize_huc(huc, edir, coordlist, cursor, db):
 
+def main(huc, edir, cursor, regen):
+    print "HUC12 is {0}".format(huc)  
+    # Get the snapped raster coordinates
+    huc_coords = huc_bounds(huc, cursor)
+    # Process the huc raster if necessary
+    if not os.path.isfile("{0}/huc12_{1}_clip.tif".format(edir, huc)) or regen:
+        rasterize_huc(huc, edir, huc_coords)
+    # Generate the huc12 summary statistics
+    #summarize_huc(huc, edir, huc_coords, cursor, db)
+
+
+
+
 ########
 # MAIN #
 ########
@@ -48,25 +61,22 @@ p = argparse.ArgumentParser(prog="huc_12_summary_statistics.py")
 p.add_argument("huc12", type=str, help="The huc_12 id to process.")
 p.add_argument("exportdir", help="The directory that holds the rasterized huc12s.")
 p.add_argument("-r", "--regen", dest="regen", required=False, action="store_true", help="Force regeneration of rasterized huc12 even if it exists.")
+p.add_argument("-l", "--list", dest="inlist", required=False, action="store_true", help="Providing a list of hucs instead of an individual huc.")
 args = p.parse_args()
 
 huc = args.huc12
 edir = args.exportdir
 
-print "HUC12 is {0}".format(huc)
 # Connect to db
 db = psycopg2.connect(host='localhost', database='blackosprey',user='jessebishop')
 cursor = db.cursor()
 
-# Get the snapped raster coordinates
-huc_coords = huc_bounds(huc, cursor)
-
-# Process the huc raster if necessary
-if not os.path.isfile("{0}/huc12_{1}_clip.tif".format(edir, huc)) or args.regen:
-    rasterize_huc(huc, edir, huc_coords)
-
-# Generate the huc12 summary statistics
-#summarize_huc(huc, edir, huc_coords, cursor, db)
+# Run it
+if args.inlist:
+    for h in open(huc, 'r'):
+        main(h.strip(), edir, cursor, args.regen)
+else:
+    main(huc, edir, cursor, args.regen)
 
 cursor.close()
 db.close()

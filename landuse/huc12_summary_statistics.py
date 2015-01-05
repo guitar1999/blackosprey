d8ba339f2e88ba9e705fd1e48866ad4ff8be7d5f
Jesse Bishop <guitar1999@aol.com>
2015-01-04 22:01:16 -0500

    all in a days work

--- landuse/huc12_summary_statistics.py
+++ landuse/huc12_summary_statistics.py
@@ -46,9 +46,10 @@ def clip_data(coordlist, infile, edir, huc):
     '''Clips a national dataset down to the huc12 extent for quick analysis'''
     xmin, ymin, xmax, ymax = coordlist
     outfile = """{0}/{1}_huc{2}.tif""".format(edir, os.path.splitext(os.path.basename(infile))[0], huc)
-    command = """/Library/Frameworks/GDAL.framework/Programs/gdalwarp -te {0} {1} {2} {3} -tr 30 30 -co "COMPRESS=LZW" {4} {5}""".format(xmin, ymin, xmax, ymax, infile, outfile)
-    proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    procout = proc.communicate()
+    if not os.path.isfile(outfile):
+        command = """/Library/Frameworks/GDAL.framework/Programs/gdalwarp -te {0} {1} {2} {3} -tr 30 30 -co "COMPRESS=LZW" {4} {5}""".format(xmin, ymin, xmax, ymax, infile, outfile)
+        proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        procout = proc.communicate()
     return outfile
 
 def summarize_huc(huc, edir, coordlist, cursor, db):
@@ -56,12 +57,12 @@ def summarize_huc(huc, edir, coordlist, cursor, db):
     # A lookup dictionary for landcover
     landcover_lookup = {"Open Water" : 11, "Perennial Ice/Snow" : 12, "Developed, Open Space" : 21, "Developed, Low Intensity" : 22, "Developed, Medium Intensity" : 23, "Developed, High Intensity" : 24, "Barren Land" : 31, "Deciduous Forest" : 41, "Evergreen Forest" : 42, "Mixed Forest" : 43, "Dwarf Scrub" : 51, "Shrub/Scrub" : 52, "Grassland/Herbaceous" : 71, "Sedge/Herbaceous" : 72, "Lichens" : 73, "Moss" : 74, "Pasture/Hay" : 81, "Cultivated Crops" : 82, "Woody Wetlands" : 90, "Emergent Herbaceous Wetlands" : 95}
     # The files to be processed
-    files = {2011 : {"lcc" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2001_to_2011_landcover_change_pixels/nlcd_2001_to_2011_landcover_change_pixels_2011_edition_2014_03_31/nlcd_2001_to_2011_landcover_change_pixels_2011_edition_2014_03_31_clip.tif", "lcft" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2001_to_2011_landcover_fromto_change_index/nlcd_2001_to_2011_landcover_fromto_change_index_2011_edition_2014_04_09/nlcd_2001_to_2011_landcover_fromto_change_index_2011_edition_2014_04_09_clip.tif", "impervious" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2011_impervious/nlcd_2011_impervious_2011_edition_2014_03_31/nlcd_2011_impervious_2011_edition_2014_03_31_clip.tif", "lc" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2011_landcover/nlcd_2011_landcover_2011_edition_2014_03_31/nlcd_2011_landcover_2011_edition_2014_03_31_clip.tif", "cd": "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2011_treecover_analytical/nlcd_2011_USFS_tree_canopy_2011_edition_2014_03_31/analytical_product/nlcd2011_usfs_treecanopy_analytical_3-31-2014_clip.tif"}, 2001 : {"lcc" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2001/change/merged_changeproduct5k_111907_clip.tif", "cd" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2001/canopy/nlcd2001_canopy_mosaic_1-29-08/nlcd_canopy_mosaic_1-29-08_clip.tif", "impervious" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2001/impervious/nlcd_2001_impervious_2011_edition_2014_03_31/nlcd_2001_impervious_2011_edition_2014_03_31_clip.tif", "lc" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2001/landcover/nlcd_2001_landcover_2011_edition_2014_03_31/nlcd_2001_landcover_2011_edition_2014_03_31_clip.tif"}, 1992 : {"lc92" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/1992/NLCD92_wall_to_wall_landcover/nlcd92mosaic_clip.tif"}}
+    files = {2011 : {"lcc" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2001_to_2011_landcover_change_pixels/nlcd_2001_to_2011_landcover_change_pixels_2011_edition_2014_03_31/nlcd_2001_to_2011_landcover_change_pixels_2011_edition_2014_03_31_clip.tif", "lcft" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2001_to_2011_landcover_fromto_change_index/nlcd_2001_to_2011_landcover_fromto_change_index_2011_edition_2014_04_09/nlcd_2001_to_2011_landcover_fromto_change_index_2011_edition_2014_04_09_clip.tif", "impervious" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2011_impervious/nlcd_2011_impervious_2011_edition_2014_03_31/nlcd_2011_impervious_2011_edition_2014_03_31_clip.tif", "lc" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2011_landcover/nlcd_2011_landcover_2011_edition_2014_03_31/nlcd_2011_landcover_2011_edition_2014_03_31_clip.tif", "cd": "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2011/nlcd_2011_treecover_analytical/nlcd_2011_USFS_tree_canopy_2011_edition_2014_03_31/analytical_product/nlcd2011_usfs_treecanopy_analytical_3-31-2014_clip.tif"}, 2001 : {"cd" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2001/canopy/nlcd2001_canopy_mosaic_1-29-08/nlcd_canopy_mosaic_1-29-08_clip.tif", "impervious" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2001/impervious/nlcd_2001_impervious_2011_edition_2014_03_31/nlcd_2001_impervious_2011_edition_2014_03_31_clip.tif", "lc" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/2001/landcover/nlcd_2001_landcover_2011_edition_2014_03_31/nlcd_2001_landcover_2011_edition_2014_03_31_clip.tif"}, 1992 : {"lc92" : "/Volumes/BlackOsprey/GIS_Data/NLCD/new/1992/NLCD92_wall_to_wall_landcover/nlcd92mosaic_clip.tif"}}
     # Set up the db record for the huc
     try:
-        query = "INSERT INTO huc_12_summary_statistics (huc_12) VALUES ({0});".format(huc)
+        query = "INSERT INTO huc_12_summary_statistics (huc_12) VALUES ('{0}');".format(huc)
         cursor.execute(query)
-    except IntegrityError, msg:
+    except psycopg2.IntegrityError, msg:
         print "HUC {0} is already in the table".format(huc)
         db.rollback()
     except Exception, msg:
@@ -74,43 +75,64 @@ def summarize_huc(huc, edir, coordlist, cursor, db):
     # Read the huc
     huc_handle = gdal.Open("{0}/huc12_{1}_clip.tif".format(edir, huc))
     hucdata = huc_handle.ReadAsArray()
+    zoneid = np.unique(hucdata)
+    zoneid = zoneid[np.nonzero(zoneid)]
+    num_pixels = np.bincount(np.int_(hucdata.reshape(hucdata.shape[0] * hucdata.shape[1])))[np.int_(zoneid)]
+    query = "UPDATE huc_12_summary_statistics SET (num_pixels) = ({0}) WHERE huc_12 = '{1}'".format(num_pixels, huc)
+    cursor.execute(query)
+    db.commit()
     # Loop through the files and do it
     for year in files.keys():
+        print year
         for dt in files[year]:
+            print dt
             f = files[year][dt]
             # Clip the file
             data_file = clip_data(coordlist, f, edir, huc)
             # Process
             if dt == "impervious" or dt == "cd":
                 # Zone is the huc
-                zoneid = np.unique(hucdata)
-                zoneid = zoneid[np.nonzero(zoneid)]
                 image_handle = gdal.Open(data_file)
                 image = image_handle.ReadAsArray()
+                if dt == 'cd' and year == 2011:
+                    image = image[0,:,:]
+                if not hucdata.shape == image.shape:
+                    print "Shape error with huc {0}!".format(huc)
+                    ef = open("{0}/{1}.error".format(edir, huc), 'w')
+                    ef.write(huc + '\n')
+                    ef.close()
+                    return
                 mean = scipy.ndimage.mean(image, labels=hucdata, index=zoneid)
                 std_dev = scipy.ndimage.standard_deviation(image, labels=hucdata, index=zoneid)
                 zmin = scipy.ndimage.minimum(image, labels=hucdata, index=zoneid)
                 zmax = scipy.ndimage.maximum(image, labels=hucdata, index=zoneid)
-                query = "UPDATE huc_12_summary_statistics SET (s{0}_{1}_mean, s{0}_{1}_std_dev, s{0}_{1}_min, s{0}_{1}_max) = ({2}, {3}, {4}, {5}) WHERE huc = {6};".format(year, dt, mean, std_dev, zmin, zmax, huc)
+                query = "UPDATE huc_12_summary_statistics SET (s{0}_{1}_mean, s{0}_{1}_std_dev, s{0}_{1}_min, s{0}_{1}_max) = ({2}, {3}, {4}, {5}) WHERE huc_12 = '{6}';".format(year, dt, mean[0], std_dev[0], zmin[0], zmax[0], huc)
             else:
                 # For DB table, need every value that exists in any of these rasters!
                 lc_handle = gdal.Open(data_file)
                 lcdata = lc_handle.ReadAsArray()
+                if not hucdata.shape == lcdata.shape:
+                    print "Shape error with huc {0}!".format(huc)
+                    ef = open("{0}/{1}.error".format(edir, huc), 'w')
+                    ef.write(huc + '\n')
+                    ef.close()
+                    return
                 # Make a mask of the huc
                 hucmask = hucdata == 0
                 np.putmask(lcdata, hucmask, 0)
                 pzone = pd.Series(lcdata.reshape(lcdata.shape[0] * lcdata.shape[1]))
-                num_pixels = pzone.value_counts()
-                num_pixels.name = 'num_pixels'
-                pix_dict = num_pixels.to_dict()
+                num_pixels_lc = pzone.value_counts()
+                num_pixels_lc.name = 'num_pixels_lc'
+                pix_dict = num_pixels_lc.to_dict()
                 cols = []
                 vals = []
                 for k,v in pix_dict.items():
-                    if k == 0:
+                    if k == 0 or ((dt == 'lc' or dt == 'lcc') and (k < 11 or k > 95)) or (dt == 'lcft' and k > 289):
                         continue
                     cols.append("s{0}_{1}_{2}".format(year, dt, k))
-                    vals.append(str(num_pixels[k]))
-                query = "UPDATE huc_12_summary_statistics SET ({0}) = {1} WHERE huc = {2};".format(','.join(cols), ','.join(vals), huc)
+                    vals.append(str(pix_dict[k]))
+                query = "UPDATE huc_12_summary_statistics SET ({0}) = ({1}) WHERE huc_12 = '{2}';".format(','.join(cols), ','.join(vals), huc)
+            #print query
             cursor.execute(query)
             db.commit()
 
@@ -144,7 +166,12 @@ def main(huc, edir, cursor, regen):
     if not os.path.isfile("{0}/huc12_{1}_clip.tif".format(edir, huc)) or regen:
         rasterize_huc(huc, edir, huc_coords)
     # Generate the huc12 summary statistics
-    #summarize_huc(huc, edir, huc_coords, cursor, db)
+    try:
+        summarize_huc(huc, edir, huc_coords, cursor, db)
+    except Exception, msg:
+        print "ERROR with huc {0}".format(huc)
+        print str(msg)
+        db.rollback()
 
 
 
---
+++ landuse/huc12_zone_dump_ned_res.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+starttime=$(date "+%s")
+
+huclist=$(psql -d blackosprey -t -A -c "SELECT distinct huc_12 FROM nhd_hu12_watersheds WHERE in_study_area = 'Y';")
+raster='/Volumes/BlackOsprey/GIS_Data/USGS/NED/1arcsec_processed/ned_study_area_1arcsec.img'
+for huc in $huclist
+do
+	echo $huc
+	# Dump the watershed
+	pgsql2shp -f /Volumes/BlackOsprey/GIS_Data/NHD/hu12_rasters/huc_${huc}_ned.shp blackosprey "SELECT dumpid, geom FROM nhd_hu12_watersheds WHERE huc_12 = '$huc';"
+
+	# Get the raster info
+	tr=$(gdalinfo $raster | grep 'Pixel Size' | awk -F ' = ' '{print $2}' | tr -d '()-' | sed 's/,/ /g')
+	ll=$(gdalinfo $raster | grep 'Lower Left' | grep  -o "\( \?-\?[0-9]\{1,3\}\.[0-9]*,[ ]*-\?[0-9]\{1,2\}\.[0-9]*\)" | tr -d '() ' | sed 's/,/ /g')
+	ur=$(gdalinfo $raster | grep 'Upper Right' | grep  -o "\( \?-\?[0-9]\{1,3\}\.[0-9]*,[ ]*-\?[0-9]\{1,2\}\.[0-9]*\)" | tr -d '() ' | sed 's/,/ /g')
+
+	# Rasterize the shapefile
+	gdal_rasterize -at -a DUMPID -a_nodata 0 -init 0 -te $ll $ur -tr $tr -co "COMPRESS=LZW" huc_${huc}_ned.shp mask_${huc}_ned.tif
+
+	# Clean up the shapefile
+	/bin/rm huc_${huc}_ned.*
+
+done
+
+endtime=$(date "+%s")
+timediff=$(echo "scale=1; ($endtime - $starttime)/60" | bc -l)
+echo "Time taken for Job ${0}: $timediff minutes on $(hostname)"
+
